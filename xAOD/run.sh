dd1e9f78546d229b017bc17daa3a95d766a0e4ac
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2018-01-24 10:18:17 -0600

    forgoten kinit

--- xAOD/run.sh
+++ xAOD/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "Setting up xAOD collector"
-
 git clone https://github.com/ATLAS-Analytics/xAOD-analytics.git
+
+echo "========= running kinit =========="
+kinit analyticssvc@CERN.CH -k -t /tmp/keytab/analyticssvc.keytab
+
 echo "========= all set up. ============"
 
 cd xAOD-analytics/container
