2d1915ab7462940595cde6ecff81892f5a39785f
Ilija Vukotic <ivukotic@uchicago.edu>
2018-02-02 05:49:12 +0100

    chmods and new runs.

--- DistributedProcessing/run_tasks_deft.sh
+++ DistributedProcessing/run_tasks_deft.sh
@@ -8,6 +8,6 @@ kinit analyticssvc@CERN.CH -k -t /tmp/keytab/analyticssvc.keytab
 
 echo "========= all set up. ============"
 
-cd DistributedProcessing/container/Jobs
+cd DistributedProcessing/container/Tasks/Enrich/DEFT/
 ls
 ./run.sh
