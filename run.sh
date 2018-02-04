78c2377b42f3c37e6b888111a2e011371a262716
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2018-02-03 18:23:09 -0600

    added Hadoop urandom option. tasks.

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ echo "========= base set up. ============"
 
 export SQOOP_HOME=/usr/local/sqoop
 export PATH=$PATH:$SQOOP_HOME/bin
+export HADOOP_OPTS=-Djava.security.egd=file:/dev/../dev/urandom
 
 echo $PATH
 ls
