6eef0437a89bae8a892d927f2538cf6b855785dd
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2018-02-19 10:21:38 -0600

    adding DDM cron. suspend everywhere

---
+++ DDM/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Setting up DDM data collector"
+git clone https://github.com/ATLAS-Analytics/DDMaccounting.git
+
+echo "========= running kinit =========="
+kinit analyticssvc@CERN.CH -k -t /tmp/keytab/analyticssvc.keytab
+
+echo "========= all set up. ============"
+
+cd DDM/import/
+ls
+echo "========== executing =========="
+./run.sh
+
+echo "All DONE."
