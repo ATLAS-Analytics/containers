134f459bd2d5b4c5bdd06796e9fea66c5718d1ce
Ilija Vukotic <ivukotic@cern.ch>
2018-03-28 14:25:14 -0500

    FTS fixes

--- FTS/run.sh
+++ FTS/run.sh
@@ -4,11 +4,13 @@ echo "Cloning FTS collector"
 
 git clone https://github.com/ivukotic/FTS_indexer.git 
 
-echo "========= running kinit =========="
-kinit analyticssvc@CERN.CH -k -t /tmp/keytab/analyticssvc.keytab
-
 echo "========= all set up. ============"
 
 cd FTS_indexer
 ls
 ./run.sh
+
+rc=$?; if [[ $rc != 0 ]]; then 
+    echo "problem with the indexer. Exiting."
+    exit $rc; 
+fi
