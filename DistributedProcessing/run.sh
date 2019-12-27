0d77b91045987c8c5fde96f3841b669cf42c7124
Ilija Vukotic <ivukotic@cern.ch>
2019-12-27 13:19:28 -0600

    remove kinit from jobs/tasks

--- DistributedProcessing/run.sh
+++ DistributedProcessing/run.sh
@@ -3,14 +3,6 @@
 echo "Setting up Distributed Processing data collector"
 git clone https://github.com/ATLAS-Analytics/DistributedProcessing.git
 
-echo "========= running kinit =========="
-kinit analyticssvc@CERN.CH -k -t /tmp/keytab/analyticssvc.keytab
-
-rc=$?; if [[ $rc != 0 ]]; then 
-    echo "problem with the kinit. Exiting."
-    exit $rc; 
-fi
-
 echo "========= all set up. ============"
 
 cd DistributedProcessing/container/$1/
